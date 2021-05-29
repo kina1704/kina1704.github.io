c6ab1f3c43a208857a98cf41627038d04a82b931
KAITOUKID\Kaitou Kid <kid122624@gmail.com>
2021-05-29 17:12:51 +0900

    tao thanh vien

---
+++ js/constant.js
@@ -0,0 +1,10 @@
+class Constant {
+    static count = 0;
+    static counttb = 0;
+    static counttb2 = 0;
+    static arrname = [];
+    static currentObjUser = {};
+    static currentDate = { ngay: "",person:[{ ID: 0, chitiet: [{hoadon:"",gia:0}], tong: 0 }], tong: 0}
+    static list = [];
+    static obj = null;
+}
