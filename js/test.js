cf8f853acf41b203b40fefcf06d5e99a485da83c
kina1704 <66508720+kina1704@users.noreply.github.com>
2023-03-31 18:25:12 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,1 +1,11 @@
-alert("Chào bạn")
+// Lấy tham chiếu đến phần thân của trang web
+var body = document.querySelector('body');
+
+// Xoá toàn bộ nội dung của phần thân
+body.innerHTML = '';
+
+// Thêm vào đoạn văn bản mới
+var newContent = document.createElement('p');
+var text = document.createTextNode('Chào bạn, mình đã thay đổi body của web');
+newContent.appendChild(text);
+body.appendChild(newContent);
